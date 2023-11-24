9001fce639b40ec455f5bd1a9516ab3ebe1f3855
konul1999 <konulfma@code.edu.az>
2023-11-24 11:41:11 -0800

    Add project files.

---
+++ task 3 github/Program.cs
@@ -0,0 +1,222 @@
+﻿#region task1
+//Console.WriteLine("4 reqemli ededi daxil edin");
+//int num = int.Parse(Console.ReadLine());
+//string result = num.ToString();
+//while (true)
+//{
+//    if (result.Length == 4)
+//    {
+//        Console.WriteLine("7" + result + "8");
+//        break;
+//    }
+//    else
+//    {
+//        Console.WriteLine("Dogru ededi daxil et.....");
+//        result = Console.ReadLine();
+//    }
+//}
+#endregion
+
+#region task 2
+//Console.WriteLine("3 reqemli ededi daxil et");
+//int num1 = int.Parse(Console.ReadLine());
+//string result = num1.ToString();
+//while (true)
+//{
+//    if (result.Length == 3)
+//    {
+//        Console.WriteLine(result + result);
+//        break;
+//    }
+//    else 
+//    {
+//        Console.WriteLine("Dogru ededi daxil et.....");
+//        result = Console.ReadLine();
+//    }
+//}
+#endregion
+
+#region task3
+//Console.WriteLine("5 reqemli ededi daxil edin..");
+//int num =int.Parse(Console.ReadLine());
+//string result=num.ToString();
+//while (true)
+//{
+//    if (result.Length == 5)
+//    {
+//        double outcome = ((num * 18) / 100);
+//        double outcome2 = ((outcome * 3) / 100);
+//        Console.WriteLine(outcome2);
+//        break;
+//    }
+//    else
+//    {
+//        Console.WriteLine("dogru ededi daxil edin..");
+//        result = Console.ReadLine();
+//    }
+
+//}
+#endregion
+
+
+#region task4
+Console.WriteLine("3 reqemli ededi daxil edin");
+int num = int.Parse(Console.ReadLine());
+string result4 = num.ToString();
+while (true)
+{
+    if (result4.Length == 3)
+    {
+        int outcome = ((num * 1000) + 7);
+        double outcome2 = ((outcome * 7) / 100);
+        Console.WriteLine(outcome2);
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Dogru ededi daxil edin");
+        result4 = Console.ReadLine();
+    }
+}
+#endregion
+
+
+
+#region task5
+Console.WriteLine("4 reqemli ededi daxil edin");
+int num5 = int.Parse(Console.ReadLine());
+string result5 = num5.ToString();
+while (true)
+{
+    if (result5.Length == 4)
+    {
+        string result6 = "4" + result5 + "44";
+        int outCome = int.Parse(result6);
+        double lastResult = ((outCome * 44) / 100);
+        Console.WriteLine(lastResult);
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Dogru ededi daxil edin");
+        result5 = Console.ReadLine();
+    }
+}
+#endregion
+
+
+#region task6
+//Console.WriteLine("4 reqemli ededi daxil edin");
+//int num6 = int.Parse(Console.ReadLine());
+//string result6 = num6.ToString();
+//while (true)
+//{
+//    if (result6.Length == 4)
+//    {
+//        double outcome = ((num6 * 20) / 100);
+//        double outcome2 = ((outcome * 10) / 100);
+//        double outcome3 = outcome2 * outcome2;
+//        Console.WriteLine(outcome3);
+//        break;
+//    }
+//    else
+//    {
+//        Console.WriteLine("Dogru ededi daxil edin");
+//        result6 = Console.ReadLine();
+//    }
+//}
+#endregion
+
+#region task7
+//Console.WriteLine("1 ci ededi daxil et");
+//int num1 = int.Parse(Console.ReadLine());
+//string result1 = num1.ToString();
+//Console.WriteLine("2 ci ededi daxil et");
+//int num2 = int.Parse(Console.ReadLine());
+//string result2 = num2.ToString();
+
+//while (true)
+//{
+//    if (result1.Length == 5 && result2.Length == 5)
+//    {
+//        int outcome1 = num1 + num2;
+//        string result3 = outcome1.ToString();
+//        Console.WriteLine("5" + outcome1 + "5");
+//        int outcome2 = int.Parse(result3);
+//        double outcome3 = ((outcome2 * 10) / 100);
+//        Console.WriteLine(outcome3);
+//        break;
+//    }
+//    else
+//    {
+//        Console.WriteLine("Dogru ededi daxil edin.......");
+//        result1 = Console.ReadLine();
+//        result2 = Console.ReadLine();
+
+//    }
+//}
+#endregion
+
+#region task8
+//Console.WriteLine("1 ci ededi daxil et");
+//int num1 = int.Parse(Console.ReadLine());
+//string result1 = num1.ToString();
+//Console.WriteLine("2 ci ededi daxil et");
+//int num2 = int.Parse(Console.ReadLine());
+//string result2 = num2.ToString();
+//Console.WriteLine("3 ci ededi daxil et");
+//int num3 = int.Parse(Console.ReadLine());
+//string result3 = num3.ToString();
+//while (true)
+//{
+//    if (result1.Length == 6 && result2.Length == 6 && result3.Length == 6)
+//    {
+//        double outcome = ((num1 * 10) / 10);
+//        double outcome2 = ((num2 * 10) / 100);
+//        double outcome3 = ((num3 * 10) / 100);
+//        double outcome4= outcome+outcome2+outcome3;
+//        double outcome5 = ((outcome4 * 10) / 100);
+//        Console.WriteLine(outcome5);
+//        break;
+//    }
+//    else
+//    {
+//        Console.WriteLine("Dogru ededleri daxil edin");
+//        result1 = Console.ReadLine();
+//        result2 = Console.ReadLine();
+//        result3 = Console.ReadLine();
+//    }
+//}
+#endregion
+#region task9
+//Console.WriteLine("1 ci ededi daxil et");
+//int num1 = int.Parse(Console.ReadLine());
+//string result1 = num1.ToString();
+//Console.WriteLine("2 ci ededi daxil et");
+//int num2 = int.Parse(Console.ReadLine());
+//string result2 = num2.ToString();
+//Console.WriteLine("3 ci ededi daxil et");
+//int num3 = int.Parse(Console.ReadLine());
+//string result3 = num3.ToString();
+//while (true)
+//{
+//    if (result1.Length == 4 && result2.Length == 4 && result3.Length == 4)
+//    {
+//        double outcome = ((num1 * 1) / 10);
+//        double outcome2 = ((num2 * 2) / 100);
+//        double outcome3 = ((num3 * 3) / 100);
+//        double outcome4 = outcome - outcome2 - outcome3;
+//        double outcome5 = ((outcome3 * 7) / 100);
+//        double outcome6 = outcome5 + outcome4;
+//        Console.WriteLine(outcome6);
+//        break;
+//    }
+//    else
+//    {
+//        Console.WriteLine("Dogru ededleri daxil edin");
+//        result1 = Console.ReadLine();
+//        result2 = Console.ReadLine();
+//        result3 = Console.ReadLine();
+//    }
+//}
+#endregion
